8d4c6e1fea840867fa6e6c94ca35fee3fa0c416e
Baglan Dosmagambetov <baglan.dos@gmail.com>
2014-05-09 19:57:21 +0600

    Initial Commit

---
+++ MCSharedCache/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MCSharedCache
+//
+//  Created by Baglan on 5/9/14.
+//  Copyright (c) 2014 MobileCreators. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MCSharedCacheTests/MCSharedCacheTests.m
@@ -0,0 +1,34 @@
+//
+//  MCSharedCacheTests.m
+//  MCSharedCacheTests
+//
+//  Created by Baglan on 5/9/14.
+//  Copyright (c) 2014 MobileCreators. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MCSharedCacheTests : XCTestCase
+
+@end
+
+@implementation MCSharedCacheTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
